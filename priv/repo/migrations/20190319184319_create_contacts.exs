da450a668fb9875f6c581315aa4a80cd0e1412ca
Ramshell <ramshellcinox@gmail.com>
2019-03-19 20:27:25 -0300

    Add contacts migration file

---
+++ priv/repo/migrations/20190319184319_create_contacts.exs
@@ -0,0 +1,12 @@
+defmodule Contacts.Repo.Migrations.CreateContacts do
+  use Ecto.Migration
+
+  def change do
+    create table(:contacts) do
+      add :name, :string
+      add :last_name, :string
+      add :email, :string
+      add :phone_number, :string
+    end
+  end
+end
